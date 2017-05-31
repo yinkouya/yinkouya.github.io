3e76aacc85c0ba38784096930b63b521f8ad608c
yinkouya <yinkouya@163.com>
2017-05-31 16:24:39 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,17 @@
 $('.carousel').carousel({
   interval: 3000;
 })
+
+
+function DrawImage(hotimg)
+{
+    $(hotimg).jqthumb({
+    classname      : 'jqthumb',
+            width          : '100%',
+            height         : '300px',
+            position       : { y: '50%', x: '50%'},
+            zoom           : '1',
+            method         : 'auto',
+    });
+}
+
