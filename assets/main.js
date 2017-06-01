6b23179508d36d783c43c4471749ef713d1062ae
yinkouya <yinkouya@163.com>
2017-06-01 21:12:00 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,5 @@ $(document).ready(function(){
     speed: 500,
     fade: true,
     cssEase: 'linear'
-    arrows: true,
-    prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
-    nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
   });
 });
