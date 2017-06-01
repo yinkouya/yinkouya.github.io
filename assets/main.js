c798cd87550097f1c0bb88bea8edaf9ee83d7ec0
yinkouya <yinkouya@163.com>
2017-06-01 20:26:04 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,13 @@
-$('.demo').slick({
-	infinite: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	dots: true,
-	speed: 500,
-	fade: true,
-	cssEase: 'linear'
-)};
+$(document).ready(function(){
+  $('.demo').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
