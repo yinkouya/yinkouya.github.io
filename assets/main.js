2840c3d215ff856abedd5cd67c1cb83f74f31a9e
yinkouya <yinkouya@163.com>
2017-06-01 19:18:29 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,11 @@
-$(document).ready(function(){
-	$('.slickcarousel').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 3000,
-                dots: true,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear',
-        )};
+$('.slickcarousel').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	dots: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear',
 )};
