35186a48a2d9cbf8135aeace74abb65b48602822
yinkouya <yinkouya@163.com>
2017-06-24 23:24:48 +0800

    update

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,1 @@
-$(document).ready(function(){
-  $('.demo').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    arrows: true,
-  });
-});
+
